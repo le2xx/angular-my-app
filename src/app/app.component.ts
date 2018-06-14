a3da6c50ddcd4b8dad5dc75c6899e1cd85f07566
le2xx <le2x@rambler.ru>
2018-06-14 16:23:51 +0300

    настройка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  msg = 'Учимся Vue а за одно и css с js';
+  menu = [
+    { example: 1, text: 'Hello World!' },
+    { example: 2, text: 'Динамически связанные свойства' },
+    { example: 3, text: 'Условия v-if' },
+    { example: 4, text: 'Циклы v-for' },
+    { example: 5, text: 'События v-on' },
+    { example: 6, text: 'Cвязывание элементов форм v-model' },
+    { example: 7, text: 'Компоненты' },
+    { example: 8, text: 'CRUD' },
+    { example: 9, text: 'Часы' }
+  ];
 }
